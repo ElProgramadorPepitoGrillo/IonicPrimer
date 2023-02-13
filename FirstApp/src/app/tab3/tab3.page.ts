5eefa6bcb9ac9c0da5b57c4f12973216f155f6c1
S3BASCH <sebaschuquiguanga@gmail.com>
2023-02-13 11:39:31 -0500

    dos

--- FirstApp/src/app/tab3/tab3.page.ts
+++ FirstApp/src/app/tab3/tab3.page.ts
@@ -13,8 +13,38 @@ export class Tab3Page {
     const alert = await this.alertController.create({
       header: 'ALERTA',
       subHeader: 'Alerta de IONIC',
-      message: 'Ya mamo la APP',
+      message: 'Ya valio la APP',
       buttons: ['OK','Cancel'],
+    });
+
+    await alert.present();
+  }
+  
+  async presenAlert() {
+    const alert = await this.alertController.create({
+      header: 'Please enter your info',
+      buttons: ['OK'],
+      inputs: [
+        {
+          placeholder: 'Name',
+        },
+        {
+          placeholder: 'Nickname (maximo 8 caracteres)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: 'number',
+          placeholder: 'edad',
+          min: 1,
+          max: 100,
+        },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
     });
 
     await alert.present();
